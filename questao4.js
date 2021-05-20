85134943180e10f994ce4d2bc23550ae41ffa3e8
Jezielle Cunha <jezielle.cunha@gmail.com>
2021-05-20 14:09:40 -0300

    Inclusão do arquivo questao4.js

---
+++ questao4.js
@@ -0,0 +1,10 @@
+//4 - Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa 60,00 reais por dia e 0,15 centavos por km rodado.
+
+let kmPercorridos = 91
+
+let diasAlugados = 3
+
+let valorAluguel = kmPercorridos*0.15 + diasAlugados*60
+
+console.log(`O valor total do aluguel do veículo é R$${valorAluguel}`)
+
