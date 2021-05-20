8ed90d99ae28727859105444afb2185c65c278ea
Jezielle Cunha <jezielle.cunha@gmail.com>
2021-05-20 14:10:16 -0300

    Inclusão do arquivo questao5.js

---
+++ questao5.js
@@ -0,0 +1,11 @@
+//5 - Receba do usuários 3 números e calcule a média entre eles.
+
+let numero1 = 6
+
+let numero2 = 9 
+
+let numero3 = 18
+
+let media = (numero1 + numero2 + numero3)/3
+
+console.log(`A média é igual a ${media}.`)
